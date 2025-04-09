22c66697e06562bd10294f399e8686aca9e74766
Kaeno Louis Eyer <pg43msk@eduvaud.ch>
2025-04-09 11:21:54 +0200

    add option to delete all Cards

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,36 +1,54 @@
 <template>
-  <h4 class="text-center">Modifiez les clés-valeurs que vous souhaitez réviser !</h4>
-    
-  <table>
-    <tr data-theme="dark">
-      <th >Clés</th>
-      <th>Valeurs</th>
-    </tr>
-    <tr v-for="item in modelValue">
-      <td>
-        <input type="text" v-model="item.key">
-      </td>
-      <td>
-        <input type="text" v-model="item.value">
-      </td>
-      <div class="flex align-baseline mt-2 ml-4">
-        <button @click="deleteItem(item)">
-          <img class="size-16 sm:size-6" src="../assets/delete.png" alt="">
-        </button>
-      </div>
-      </tr>
-  </table>
-  
-  <div class="flex justify-center m-10 gap-5">
-    <button @click="resetChanges">Annuler</button>
-    <button :disabled="!verifyChangeInData()" @click="saveChanges">Sauvegarder</button>
+  <div v-if="gestConfirmDelete === 'show'">
+    <ConfirmDelete v-model:confirmDelete="gestConfirmDelete"/>
   </div>
-</template>
+  <div v-else>
+    <h4 class="text-center">Modifiez les clés-valeurs que vous souhaitez réviser !</h4>
+    
+    <table>
+      <tr data-theme="dark">
+        <th >Clés</th>
+        <th>Valeurs</th>
+        <th>Supprimer 
+          <span class="ml-2"><input @click="gestConfirmDelete = 'show'" type="checkbox">
+          </span>
+        </th>
+        
+        <div class="">
+        </div>
+      </tr>
+      <tr v-for="item in modelValue">
+        <td>
+          <input type="text" v-model="item.key">
+        </td>
+        <td>
+          <input type="text" v-model="item.value">
+        </td>
+        <td>
+          <div class="flex justify-center align-middle">
+            <button @click="deleteItem(item)">
+              <img class="size-16 sm:size-6" :src="imgDelete" alt="">
+            </button>
+          </div>
+        </td>
+      </tr>
+    </table>
+    
+    <div class="flex justify-center m-10 gap-5">
+      <button @click="resetChanges">Annuler</button>
+      <button :disabled="!verifyChangeInData()" @click="saveChanges">Sauvegarder</button>
+    </div>
+  </div>
+  </template>
 
 <!-- ******************************** SCRIPT PART ******************************** -->
 
 <script setup>
 import { ref, watch } from 'vue'
+import imgDelete from '../assets/delete.png'
+import ConfirmDelete from './ConfirmDelete.vue'
+
+const gestConfirmDelete = ref('no')
 const modelValue = defineModel({ type: Array})
 const modify = defineModel('modify')
 
@@ -51,6 +69,13 @@ const resetChanges = () => {
 watch(modelValue, (newVal) => {
   hasChanged.value = JSON.stringify(newVal) !== JSON.stringify(originalData.value)
 }, { deep: true })
+
+watch(gestConfirmDelete, (val) => {
+  if (val === 'yes') {
+    modelValue.value = []
+    modify.value = false
+  }
+})
 
 const saveChanges = () => {
   originalData.value = JSON.parse(JSON.stringify(modelValue.value))
